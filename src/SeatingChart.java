0ddb9ba801bf5c5aa5ab0cdd109ce3124869a6b8
pat11144 <128dpatel@frhsd.com>
2026-09-15 14:01:07 -0400

    adding in main class

--- src/SeatingChart.java
+++ src/SeatingChart.java
@@ -2,13 +2,33 @@ import java.util.*;
 public class SeatingChart {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your name:");
-        String name = sc.nextLine();
         System.out.println("Enter the number of absences.");
         int absences = sc.nextInt();
 
-        Student s = new Student(name, absences);
+        Student a = new Student("Jayden", 0);
+        Student b = new Student("Max", 0);
+        Student c = new Student("Bob", 0);
+        Student d = new Student("jay", 0);
+        Student e = new Student("Avery", 0);
+        Student f = new Student("Emily", 0);
+        Student g = new Student("Ava", 0);
+        Student h = new Student("D", 0);
         Student[][] seats = new Student[3][4];
+        seats[0][0] = a;
+        seats[0][3] = b;
+        seats[1][1] = c;
+        seats[2][3] = d;
+        seats[1][3] = e;
+        seats[2][3] = f;
+        seats[1][2] = g;
+        seats[1][3] = h;
+
+
+        for(int i = 0; i < seats.length; i++){
+            for(int j = 0; j < seats[i].length; j++){
+
+            }
+        }
 
 
     }
